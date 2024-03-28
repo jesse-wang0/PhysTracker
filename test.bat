d2f9dd3581570d49e8a9d50aeaff6a8296a7365e
jesse <jesse21532@gmail.com>
2024-03-28 20:57:35 +1000

    Added some tests on failing cases

--- test.bat
+++ test.bat
@@ -1,1 +1,6 @@
+py extract_frame.py -i "..\example2\video\fake.mp4" -o "..\example2\empty_frames"
+
 py extract_frame.py -i "..\example2\video\Bouncing ball reference.mp4" -o "..\example2\frames"
+py extract_frame.py -i "..\example2\video\Bouncing ball reference.mp4" -o "..\example2\nonempty_frames" -f
+
+py extract_frame.py -i "..\example2\video\Bouncing ball reference.mp4" -o "..\example2\zxczxczx"
