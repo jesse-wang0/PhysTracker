b54ddfeca22cb240514e112f42233c36e9ea061e
jesse <jesse21532@gmail.com>
2024-04-04 16:26:34 +1000

    Successfully runs CL tool and can take all optional args

--- gui.py
+++ gui.py
@@ -1,30 +1,61 @@
 import tkinter as tk
 from tkinter import filedialog
+import subprocess
+import shlex
 
-global input_path
-global output_path
+global input_path, output_path, force, skip
 input_path = ""
 output_path = ""
+force = False
+skip = 1
+
+def get_subprocess_output(cmd):
+    args = shlex.split(cmd)
+
+    proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = proc.communicate()
+    exitcode = proc.returncode
+    return exitcode, out, err
 
 def input_dialog():
     global input_path
     path = filedialog.askopenfilename()
-    selected_input_label.config(text=f"Selected File: {path}")
     if path:
+        selected_input_label.config(text=f"Selected File: {path}")
         label_input_error.config(text="")
         input_path = path
 
 def output_dialog():
     global output_path
     path = filedialog.askdirectory()
-    selected_output_label.config(text=f"Selected Directory: {path}")
     if path:
+        selected_output_label.config(text=f"Selected Directory: {path}")
         label_output_error.config(text="")
         output_path = path
 
+def apply_force():
+    global force
+    force = not force
+    print(force)
+
+def skip_frame():
+    global skip
+    skip = spinbox_skip.get()
+
 def process_file():
+    exitcode = 1
     if input_path and output_path:
-        print("nice")
+        if force:
+            result = subprocess.run(["py", "extract_frame.py", "-i", input_path, 
+                                    "-o", output_path, "-f"])
+        else:
+            result = subprocess.run(["py", "extract_frame.py", "-i", input_path, 
+                                    "-o", output_path])
+        exitcode = result.returncode
+        if not exitcode:
+            label_success.config(text="Process Successful", fg="green")
+        else:
+            label_success.config(text="Unsuccessful", fg="red")
     else:
         if not input_path:
             label_input_error.config(text="Please provide input path", fg="red")
@@ -34,8 +65,9 @@ def process_file():
 root = tk.Tk()
 root.title("app test")
 
-label = tk.Label(text="Image Analysis")
-label.pack()
+frame_title = tk.Frame(relief="groove", bg="light gray")
+label = tk.Label(frame_title, text="Image Analysis Tool", font=("Arial", 20), bg="light gray")
+label.pack(pady=15)
 
 frame_input = tk.Frame()
 input_button = tk.Button(frame_input, text="Open Video File", command=input_dialog)
@@ -50,17 +82,33 @@ selected_output_label = tk.Label(frame_output, text="Selected Directory:")
 selected_output_label.pack()
 
 frame_process = tk.Frame()
+test = tk.Frame(frame_process)
+label_options = tk.Label(frame_process, text="OPTIONS:", font=("Arial", 12))
+label_options.pack(pady=(20,0))
+label_force = tk.Label(test, text="Force:")
+label_force.pack(side=tk.LEFT)
+check_force = tk.Checkbutton(test, command=apply_force)
+check_force.pack(side=tk.LEFT)
+label_skip = tk.Label(test, text="Skip frame:")
+label_skip.pack(padx=(30,0), side=tk.LEFT)
+spinbox_skip = tk.Spinbox(test, from_= 1, to = 10, width=10, command=skip_frame) 
+spinbox_skip.pack(side=tk.LEFT)
+test.pack()
+
 start_button = tk.Button(frame_process, text="Start Processing", command=process_file)
-start_button.pack(padx=20, pady=(20,0))
+start_button.pack(padx=20, pady=(10,0))
+label_success = tk.Label(frame_process)
+label_success.pack()
 label_input_error = tk.Label(frame_process)
 label_input_error.pack()
 label_output_error = tk.Label(frame_process)
 label_output_error.pack()
 
-frame_input.pack()
+frame_title.pack(fill=tk.BOTH, expand=tk.TRUE)
+frame_input.pack(fill=tk.BOTH)
 frame_output.pack()
 frame_process.pack()
 
-root.geometry("400x300")
+root.geometry("600x400")
 
 root.mainloop()
