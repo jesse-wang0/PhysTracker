0323213e5a7ca75d7ad159fc605c5625cf876c60
jesse <jesse21532@gmail.com>
2024-06-23 19:02:20 +1200

    Fixed up formatting to ensure lines are no longer than 80 chars long

--- combine_images.py
+++ combine_images.py
@@ -1,7 +1,7 @@
 import sys, os
 import argparse
 import pathlib
-import cv2 as cv
+import cv2
 from PIL import Image
 
 def is_dir_empty(path):
@@ -18,15 +18,17 @@ def is_dir_empty(path):
         return next(scan, None) is None
     
 
-def combine_images(input_path, output_path, force_flag):
-    '''Takes frames and computes difference between them, and combines these differences into one image.
+def combine_images(input_path, output_path, threshold, force_flag):
+    """Takes frames and computes difference between them, and 
+    combines these differences into one image.
 
     Parameters:
         video_path(str): Path to frames.
         output_path(str): Path to dir to output difference frames.       
-    '''
-    input_abs_path = input_path[0].resolve()
-    output_abs_path = output_path[0].resolve()
+    """
+    input_abs_path = input_path.resolve()
+    output_abs_path = output_path.resolve()
+
     img_files = os.listdir(input_abs_path)
     if len(img_files) < 1:
         raise IOError("Frames folder is empty")
@@ -39,45 +41,44 @@ def combine_images(input_path, output_path, force_flag):
         raise FileNotFoundError(f"{output_abs_path} does not exist.")
 
     for i in range(0, len(img_files)-1):
-        img1 = cv.imread(f"{input_abs_path}/{img_files[i]}")
-        img2 = cv.imread(f"{input_abs_path}/{img_files[i+1]}")
-        difference = cv.absdiff(img1, img2)
+        img1 = cv2.imread(f"{input_abs_path}/{img_files[i]}")
+        img2 = cv2.imread(f"{input_abs_path}/{img_files[i+1]}")
 
-        greyscaled = cv.cvtColor(difference, cv.COLOR_BGR2GRAY)
-        ret, mask = cv.threshold(greyscaled, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
-        difference[mask != 255] = [0, 0, 255] #might want to make a parmater
-        cv.imwrite(f"{output_abs_path}/diff{i}.png", difference)
+        difference = cv2.absdiff(img1, img2)
+        cv2.cvtColor(difference, cv2.COLOR_BGR2GRAY)
+        cv2.imwrite(f"{output_abs_path}/diff{i}.png", difference)
+
+    img = cv2.imread(f"{output_abs_path}/diff0.png", 0)
     
-    img = cv.imread(f"{output_abs_path}/diff0.png", 0)
-    
-    total_list = []
     for x in range(0, len(img_files)-1): 
         list_trial = []
         img_name = f"{output_abs_path}/diff{x}.png"
-        print('Processing:', img_name)
-        nxt = cv.imread(img_name, 0)
+        print("Processing: ", img_name)
+        nxt = cv2.imread(img_name, 0)
         for i, row in enumerate(img):
             for j, pixel in enumerate(row):
                 d = int(img[i][j]) - int(nxt[i][j])
-                if abs(d) > 60: #2 successive frames, mayeb another tool to calc
+                if abs(d) > threshold:
                     img[i][j] = 255
                     list_trial.append((i,j))
                 else:
                     img[i][j] = (int(img[i][j]) + int(nxt[i][j]))/2
-        os.remove(f"{output_abs_path}/diff{x}.png")
+        #os.remove(f"{output_abs_path}/diff{x}.png")
 
     for i, row in enumerate(img):
         for j, pixel in enumerate(row):
             if img[i][j] < 255:
                 img[i][j] = 0
-    cv.imwrite(f"{output_abs_path}/final.png", img)
+    cv2.imwrite(f"{output_abs_path}/final.png", img)
     print("Image processing sucessful")
 
 def init_argparse() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
-        usage="""%(prog)s [[-v|--version] | [-h|--help]] 
-        | [-i|--indir <input-file-path> -o|--outdir <output-file-path>] 
-        | [-f|--force]""",
+        usage=(
+            "%(prog)s [[-v|--version] | [-h|--help]] " 
+            "| [-i|--indir <input-file-path> -o|--outdir <output-file-path>] "
+            "| [-t|--threshold] | [-f|--force]"
+        ),
         description="Takes frames and combines them into one photo"
     )
     parser.add_argument(
@@ -85,16 +86,21 @@ def init_argparse() -> argparse.ArgumentParser:
         version=f"{parser.prog} version 1.0.0"
     )
     parser.add_argument(
-        "-i", "--indir", action="store", nargs=1, type=pathlib.Path,
-        help = "Full path to directory containing frames"
+        "-i", "--indir", action="store", type=pathlib.Path,
+        help = "Full path to directory containing frames."
     )
     parser.add_argument(
-        "-o", "--outdir", action="store", nargs=1, type=pathlib.Path,
+        "-o", "--outdir", action="store", type=pathlib.Path,
         help = "Directory to store image differences."
-    )   
+    )
+    parser.add_argument(
+        "-t", "--threshold", action="store", type=int,
+        help = "Threshold value calculated by threshold tool."
+    )
     parser.add_argument(
         "-f", "--force", action=argparse.BooleanOptionalAction, type=bool,
-        help = "Force writes to directory with pre-existing files and overwrites old files."
+        help = """Force writes to directory with pre-existing files \
+                    and overwrites old files."""
     )
     return parser
 
@@ -102,7 +108,7 @@ def main() -> None:
     parser = init_argparse()
     args = parser.parse_args()
     try:
-        combine_images(args.indir, args.outdir, args.force)
+        combine_images(args.indir, args.outdir, args.threshold, args.force)
         exit(0)
     except Exception as err:
         print(err)
--- extract_frame.py
+++ extract_frame.py
@@ -1,7 +1,4 @@
-import sys, os
-import argparse
-import pathlib
-import cv2 as cv
+import sys, os, argparse, pathlib, cv2
 from PIL import Image
 from io import BytesIO
 
@@ -9,7 +6,7 @@ class ExtractFrameException(Exception):
     pass
 
 def get_first_image(path):
-        vidcap = cv.VideoCapture(path)
+        vidcap = cv2.VideoCapture(path)
         success, image = vidcap.read()
         if success:
             return Image.open(BytesIO(image))
@@ -28,14 +25,15 @@ def is_dir_empty(path):
         return next(scan, None) is None
     
 def to_image(video_path, output_path, force_flag, frame_skip):
-    '''Converts mp4 video into individual frames and stores in provided output path.
+    '''Converts mp4 video into individual frames and stores in 
+    provided output path.
 
     Parameters:
         video_path(str): Path to video.
         output_path(str): Path to output dir.
     '''
-    input_abs_path = video_path[0].resolve()
-    output_abs_path = output_path[0].resolve()
+    input_abs_path = str(video_path.resolve())
+    output_abs_path = str(output_path.resolve())
     if not str(input_abs_path).endswith(".mp4"):
         raise ValueError(f"Input is not an mp4 file.")
 
@@ -51,37 +49,52 @@ def to_image(video_path, output_path, force_flag, frame_skip):
     else:
         raise FileNotFoundError(f"{output_abs_path} does not exist.")
 
-    vid = cv.VideoCapture(str(input_abs_path))
+    vid = cv2.VideoCapture(str(input_abs_path))
 
-    (major_ver, minor_ver, subminor_ver) = (cv.__version__).split('.')
+    (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
     if int(major_ver)  < 3 :
-        fps = vid.get(cv.cv.CV_CAP_PROP_FPS)
+        fps = vid.get(cv2.cv.CV_CAP_PROP_FPS)
     else :
-        fps = vid.get(cv.CAP_PROP_FPS)
+        fps = vid.get(cv2.CAP_PROP_FPS)
     frame_delta_t = frame_skip/fps #how far apart the frames are
 
     count = 0
+    result_average = None
     while True:
         try:
             read_success, image = vid.read()
+            if result_average is None:
+                result_average = image.copy()
+            else:
+                if image is not None:
+                    result_average = result_average * (count / (count+1)) + image * (1 / (count+1))
+
             if not read_success:
                 if count <= 0:
-                    raise ExtractFrameException(f"Unable to read any frames from {input_abs_path}.")
+                    raise ExtractFrameException(f"""Unable to read any 
+                                                frames from {input_abs_path}.""")
                 break #TODO: how do we detect error in vid.read()
             print('Read a new frame: ', read_success, file=sys.stderr)
             if count%frame_skip == 0:
                 write_path = f"{output_abs_path}/{str(count).zfill(5)}.jpg"
-                write_success = cv.imwrite(write_path, image) # save frame as JPEG file
+                write_success = cv2.imwrite(write_path, image) # save as JPEG
             if not write_success:
                 raise ExtractFrameException(f"Unable to save to jpg.")
         except Exception as e:
-            raise ExtractFrameException(f"Exception converting image to jpg format. {e}")
+            raise ExtractFrameException(f"""Exception converting image \
+                                        jpg format. {e}""")
         count += 1
+    print(result_average)
+    cv2.imwrite(f"{output_abs_path}/average.jpg", result_average)
     return fps, frame_delta_t
 
 def init_argparse() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
-        usage="%(prog)s [[-v|--version] | [-h|--help]] | [-i|--infile <input-file-path> -o|--outdir <output-file-path>] | [-f|--force] | [-s|--skip]",
+        usage=(
+            "%(prog)s [[-v|--version] | [-h|--help]] "
+            "| [-i|--infile <input-file-path> -o|--outdir <output-file-path>] "
+            "| [-f|--force] | [-s|--skip]"
+        ),
         description="Converts mp4 video into photo"
     )
     parser.add_argument(
@@ -89,16 +102,18 @@ def init_argparse() -> argparse.ArgumentParser:
         version=f"{parser.prog} version 1.0.0"
     )
     parser.add_argument(
-        "-i", "--infile", action="store", nargs=1, type=pathlib.Path,
+        "-i", "--infile", action="store", type=pathlib.Path,
         help = "Full path to an mp4 file"
     )
     parser.add_argument(
-        "-o", "--outdir", action="store", nargs=1, type=pathlib.Path,
-        help = "Empty directory to store frames in. Output = nnnn.jpg and starts from 0000 onwards"
+        "-o", "--outdir", action="store", type=pathlib.Path,
+        help = """Empty directory to store frames in. Output = nnnn.jpg \
+                    and starts from 0000 onwards"""
     )
     parser.add_argument(
         "-f", "--force", action=argparse.BooleanOptionalAction, type=bool,
-        help = "Force writes to directory with pre-existing files and overwrites old files."
+        help = """Force writes to directory with pre-existing files \
+                    and overwrites old files."""
     )
     parser.add_argument(
         "-s", "--skip", action="store", default="1", type=int,
@@ -110,7 +125,8 @@ def main() -> None:
     parser = init_argparse()
     args = parser.parse_args()
     try:
-        fps, frame_delta_t = to_image(args.infile, args.outdir, args.force, args.skip)
+        fps, frame_delta_t = to_image(args.infile, args.outdir, 
+                                        args.force, args.skip)
         print(f"Skip frames: {args.skip}")
         print(f"frame_rate = {fps}")
         print(f"frame_delta_t = {frame_delta_t}")
