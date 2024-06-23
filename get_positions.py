eb8fc091b3a81f4901f90d499e9bb483b845fa12
jesse <jesse21532@gmail.com>
2024-06-24 01:31:56 +1200

    finsihed functionality for calculating velocity and position and outputing it as a graph

--- get_positions.py
+++ get_positions.py
@@ -1,6 +1,7 @@
-import cv2, os
+import cv2, os, math
 import numpy as np
 from blob_detection import setup_detector
+import matplotlib.pyplot as plt
 
 input_abs_path = "C:\\Users\\jesse\\git_projects\\example2\\testing_frames"
 average_background_path = "C:\\Users\\jesse\\git_projects\\example2\\testing_frames\\average.jpg"
@@ -11,6 +12,7 @@ frame_duration = 0.041666666666666664
 
 x_list = []
 y_list = []
+image_height = average_background.shape[0]
 
 for i in img_files:
     img = cv2.imread(f"{input_abs_path}/{i}")
@@ -38,12 +40,62 @@ for i in img_files:
                 max_area_point = key_point
     if max_area_point is not None:
         x_list.append(max_area_point.pt[0])
-        y_list.append(max_area_point.pt[1])
-
+        y_list.append(image_height - max_area_point.pt[1])
 
 #position -x
+def plot_x():
+    x_coord = np.array(x_list)
+    time = np.zeros(len(x_list))
+    for i in range(len(x_list)):
+        time[i] = frame_duration * i
+    plt.plot(time, x_coord, 'o')
+    plt.xlabel('Time (seconds)')
+    plt.ylabel('X Coordinate')
+    plt.title('X Coordinate vs Time')
+
 #position - y
+def plot_y():
+    y_coord = np.array(y_list)
+    time = np.zeros(len(y_list))
+    for i in range(len(y_list)):
+        time[i] = frame_duration * i
+    plt.plot(time, y_coord, 'o')
+    plt.xlabel('Time (seconds)')
+    plt.ylabel('Y Coordinate')
+    plt.title('Y Coordinate vs Time')
+
 #velocity - x
+def plot_x_velocity():
+    x_velocity = np.zeros(len(x_list) - 1)
+    for i in range(len(x_list) - 1):
+        velocity = (x_list[i+1] - x_list[i]) / frame_duration 
+        x_velocity[i] = velocity
+    time = np.zeros(len(x_list) - 1)
+    for i in range(1, len(x_list)):
+        time[i-1] = frame_duration * i
+    plt.plot(time, x_velocity, 'o')
+    plt.xlabel('Time (seconds)')
+    plt.ylabel('X Velocity')
+    plt.title('X Velocity vs Time')
+
 #velocity -y
+def plot_y_velocity():
+    y_velocity = np.zeros(len(y_list) - 1)
+    for i in range(len(y_list) - 1):
+        velocity = (y_list[i+1] - y_list[i]) / frame_duration 
+        y_velocity[i] = velocity
+    time = np.zeros(len(y_list) - 1)
+    for i in range(1, len(y_list)):
+        time[i-1] = frame_duration * i
+    plt.plot(time, y_velocity, 'o')
+    plt.xlabel('Time (seconds)')
+    plt.ylabel('X Velocity')
+    plt.title('X Velocity vs Time')
+
 #acceleration - x
+
 #acceleration - y
+
+plot_y_velocity()
+plt.grid(True)
+plt.show()
