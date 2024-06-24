5ef40b7c4621bd73aa4ac0f4529c38f1eb9d696a
jesse <jesse21532@gmail.com>
2024-06-24 23:19:06 +1200

    Converted into CLI tool. Changed functionality, so it will output the table of x,y and time values as a table to console. PRobably will also output as csv file, depending on if gui requires it

--- get_positions.py
+++ get_positions.py
@@ -1,101 +1,105 @@
-import cv2, os, math
-import numpy as np
+import cv2, os, argparse, pathlib, sys
+from tabulate import tabulate
 from blob_detection import setup_detector
-import matplotlib.pyplot as plt
 
-input_abs_path = "C:\\Users\\jesse\\git_projects\\example2\\testing_frames"
-average_background_path = "C:\\Users\\jesse\\git_projects\\example2\\testing_frames\\average.jpg"
-average_background = cv2.imread(average_background_path)
-img_files = os.listdir(input_abs_path)
-fps = 24.0
-frame_duration = 0.041666666666666664
+def get_positions(frames_path, avg_img_path, frame_duration, 
+                  roi, meters_per_pixel):
+    avg_background_path = str(avg_img_path.resolve())
+    average_background = cv2.imread(avg_background_path)
+    input_path = str(frames_path.resolve())
+    img_paths = os.listdir(input_path)
 
-x_list = []
-y_list = []
-image_height = average_background.shape[0]
+    x_coords = []
+    y_coords = []
+    image_height = average_background.shape[0]
 
-for i in img_files:
-    img = cv2.imread(f"{input_abs_path}/{i}")
-    difference = cv2.absdiff(img, average_background)
-    difference = cv2.cvtColor(difference, cv2.COLOR_BGR2GRAY)
-    f_, thresholded_diff = cv2.threshold(difference, 15, 255, cv2.THRESH_BINARY)
+    for path in img_paths:
+        img = cv2.imread(f"{input_path}/{path}")
+        difference = cv2.absdiff(img, average_background)
+        difference = cv2.cvtColor(difference, cv2.COLOR_BGR2GRAY)
+        f_, thresholded_diff = cv2.threshold(difference, 15, 255, 
+                                             cv2.THRESH_BINARY)
 
-    detector = setup_detector(min_area=200, max_area=2000, circularity=0.3, 
-                                convexity=0.1, inertia=0.01)
-    keypoints = detector.detect(thresholded_diff)
+        detector = setup_detector(min_area=200, max_area=2000, circularity=0.3, 
+                                  convexity=0.1, inertia=0.01)
+        keypoints = detector.detect(thresholded_diff)
 
-    colour = (0,0,255)
-    flag_match = cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS
-    img_with_keypoints = cv2.drawKeypoints(difference, keypoints, np.array([]), 
-                                            colour, flag_match)
+        max_area = 0
+        max_area_point = None
+        for key_point in keypoints:
+            x = key_point.pt[0]
+            y = key_point.pt[1]
+            size = key_point.size
+
+            x1 = roi[0]
+            x2 = roi[1]
+            y1 = roi[2]
+            y2 = roi[3]
+            if x1 < x < x2 and y1 < y < y2:
+                if size > max_area:
+                    max_area_point = key_point
+        if max_area_point is not None:
+            x_coords.append(max_area_point.pt[0] * meters_per_pixel)
+            y_coords.append((image_height - max_area_point.pt[1]) 
+                             * meters_per_pixel)
     
-    max_area = 0
-    max_area_point = None
-    for key_point in keypoints:
-        x = key_point.pt[0]
-        y = key_point.pt[1]
-        s = key_point.size
-        if 164 < x < 1187 and 57 < y < 720:
-            if s > max_area:
-                max_area_point = key_point
-    if max_area_point is not None:
-        x_list.append(max_area_point.pt[0])
-        y_list.append(image_height - max_area_point.pt[1])
+    headers = ["Time (seconds)", "x (meters)", "y (meters)"]
+    rows = []
+    #just used x_coords, as both x and y lists are same size
+    for i in range(len(x_coords)): 
+        rows.append([i*frame_duration, x_coords[i], y_coords[i]])
+    table = tabulate(rows, headers, tablefmt="grid")
+    print(table)
 
-#position -x
-def plot_x():
-    x_coord = np.array(x_list)
-    time = np.zeros(len(x_list))
-    for i in range(len(x_list)):
-        time[i] = frame_duration * i
-    plt.plot(time, x_coord, 'o')
-    plt.xlabel('Time (seconds)')
-    plt.ylabel('X Coordinate')
-    plt.title('X Coordinate vs Time')
+def tuple_type(strings):
+    strings = strings.replace("(", "").replace(")", "")
+    mapped_int = map(int, strings.split(","))
+    return tuple(mapped_int)
 
-#position - y
-def plot_y():
-    y_coord = np.array(y_list)
-    time = np.zeros(len(y_list))
-    for i in range(len(y_list)):
-        time[i] = frame_duration * i
-    plt.plot(time, y_coord, 'o')
-    plt.xlabel('Time (seconds)')
-    plt.ylabel('Y Coordinate')
-    plt.title('Y Coordinate vs Time')
+def init_argparse() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        usage=(
+            "%(prog)s [[-v|--version] | [-h|--help]] "
+            "| [-i|--infile <input-file-path>]"
+        ),
+        description="Converts mp4 video into photo"
+    )
+    parser.add_argument(
+        "-v", "--version", action="version",
+        version=f"{parser.prog} version 1.0.0"
+    )
+    parser.add_argument(
+        "-i", "--indir", action="store", type=pathlib.Path,
+        help = "Full path to input directory containing compacted frames"
+    )
+    parser.add_argument(
+        "-a", "--avg_img", action="store", type=pathlib.Path,
+        help = "Full path to average image that will be used as base comparison"
+    )
+    parser.add_argument(
+        "-d", "--duration_frame", action="store", type=float,
+        help = "Duration of time apart of each frame in seconds"
+    )
+    parser.add_argument(
+        "-r", "--roi", action="store", type=tuple_type,
+        help = "Region of interest coordinates as tuple (x1, x2, y1, y2)"
+    )
+    parser.add_argument(
+        "-m", "--meter_per_pixel", action="store", type=float,
+        help = "Meter per pixel conversion factor"
+    )
+    return parser
 
-#velocity - x
-def plot_x_velocity():
-    x_velocity = np.zeros(len(x_list) - 1)
-    for i in range(len(x_list) - 1):
-        velocity = (x_list[i+1] - x_list[i]) / frame_duration 
-        x_velocity[i] = velocity
-    time = np.zeros(len(x_list) - 1)
-    for i in range(1, len(x_list)):
-        time[i-1] = frame_duration * i
-    plt.plot(time, x_velocity, 'o')
-    plt.xlabel('Time (seconds)')
-    plt.ylabel('X Velocity')
-    plt.title('X Velocity vs Time')
+def main() -> None:
+    parser = init_argparse()
+    args = parser.parse_args()
+    try:
+        get_positions(args.indir, args.avg_img, args.duration_frame, 
+                      args.roi, args.meter_per_pixel)
+        exit(0)
+    except Exception as err:
+        print(err, file=sys.stderr)
+        exit(1)
 
-#velocity -y
-def plot_y_velocity():
-    y_velocity = np.zeros(len(y_list) - 1)
-    for i in range(len(y_list) - 1):
-        velocity = (y_list[i+1] - y_list[i]) / frame_duration 
-        y_velocity[i] = velocity
-    time = np.zeros(len(y_list) - 1)
-    for i in range(1, len(y_list)):
-        time[i-1] = frame_duration * i
-    plt.plot(time, y_velocity, 'o')
-    plt.xlabel('Time (seconds)')
-    plt.ylabel('X Velocity')
-    plt.title('X Velocity vs Time')
-
-#acceleration - x
-
-#acceleration - y
-
-plot_y_velocity()
-plt.grid(True)
-plt.show()
+if __name__ == "__main__":
+    main()
