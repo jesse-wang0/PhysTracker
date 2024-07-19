463964d09319051e09aa7e95266fc0c1e6267f61
jesse <jesse21532@gmail.com>
2024-07-19 17:29:01 +1000

    Redid page 2 to use multiprocessing instead of Subprocess. This means that when we use pyinstaller to convert into app, the python in the app can run the processes instead of relying on outside python which the user might not have

--- gui/gui_pages/page2.py
+++ gui/gui_pages/page2.py
@@ -1,8 +1,21 @@
+import os
+import sys
+import multiprocessing
+from multiprocessing import Process, Queue
+from queue import Empty
 import tkinter as tk
 from tkinter import filedialog
-import os, subprocess, sys
-from ttkbootstrap.constants import *
+import cv2
 import ttkbootstrap as tb
+from ttkbootstrap.constants import *
+from pathlib import Path
+
+current_directory = os.path.dirname(sys.path[0])
+if current_directory not in sys.path:
+    sys.path.append(current_directory)
+
+from extract_frame_cli.extract_frame import extract_frame
+from thresholding_cli.thresholding import calculate_threshold
 
 class Page2(tk.Frame):
     def __init__(self, parent, control_btns, vid_manager, **kwargs):
@@ -17,8 +30,6 @@ class Page2(tk.Frame):
         self.extraction_container = tk.Frame(self, highlightthickness=1, 
                                                    highlightbackground="black")
         self.extraction_container.pack(side=tk.TOP, fill=tk.X) 
-
-        #Title Label
         tb.Label(self.extraction_container, 
                  text="1. Frame Extraction").pack(pady=10)
         
@@ -27,10 +38,17 @@ class Page2(tk.Frame):
                                            bootstyle="outline",
                                            command=self.next_step)
         self.select_dir_button.pack(pady=(0,10))
-        
+    
+        self.extraction_process_flag = False
+        self.extraction_error_flag = False
+        self.threshold_process_flag = False
+        self.threshold_error_flag = False
+        self.queue = Queue()
+
     def next_step(self):
         path = filedialog.askdirectory()
         if path:
+            self.vid_manager.set_output_path(path)
             self.output_path = path
             self.directory_label = tk.Label(self.extraction_container, 
                      text=f"Selected Directory: {path}")
@@ -38,10 +56,9 @@ class Page2(tk.Frame):
 
             self.process_button = tb.Button(self.extraction_container, 
                                         text='Process', bootstyle="outline",
-                                        command=self.extract_and_threshold)
+                                        command=self.extract_frames)
             self.process_button['state'] = 'disabled'
             self.process_button.pack(side=tk.TOP, pady=10)
-
             self.setup_progress_bar()
 
             self.warning_msg = tk.Label(self.extraction_container)
@@ -83,40 +100,92 @@ class Page2(tk.Frame):
         else:
             self.process_button['state'] = 'disabled'
 
-    def extract_and_threshold(self):
-        exitcode_extract = self.frame_extraction()
-        threshold, exitcode_threshold = self.get_threshold()
-        self.threshold = threshold
-        if not exitcode_extract:
-            self.output_msg.config(text="Process successful", fg="green")
-        else:
-            self.output_msg.config(text="Process unsuccessful", fg="red")
 
-    def frame_extraction(self):
-        exitcode = 1
+    def extract_frames(self):
         input_path = self.vid_manager.get_vid_path()
-        output_path = self.vid_manager.get_output_path()
+        output_path = self.output_path
         force = self.force_value.get()
         skip_num = self.vid_manager.get_skip()
-        print(skip_num)
-
+        
         if input_path and output_path:
-            python_executable = sys.executable
-            command = [python_executable, "extract_frame.py", "-i", input_path,
-                       "-o", output_path, "-s", str(skip_num)]
+            args = [Path(input_path), Path(output_path), skip_num]
             if force:
-                command.append("-f")
-            result = subprocess.Popen(command)
-            exitcode = result.returncode
-            return exitcode
-        elif not output_path:
-                self.output_msg.config(text="Output path invalid", fg="red")
+                args.append(True)
+            args.append(self.queue)
+            p1 = Process(target=extract_frame, args=tuple(args)) #multiprocessing
+            p1.start()
+            self.check_extraction_status()
 
-    def get_threshold(self):
-        python_executable = sys.executable
-        img1, dimension1 = self.vid_manager.get_roi(1)
-        img2, dimension2 = self.vid_manager.get_roi(2)
-        result = subprocess.Popen([python_executable, "thresholding.py", 
-                                   "-i", img1, "-j", img2, 
-                                   "-x", dimension1, "-y", dimension2])
-        return result, result.returncode
+            roi1 = self.vid_manager.get_roi(1)[0]
+            result = cv2.imwrite(f"{self.output_path}{os.sep}roi1.png", roi1)
+            print(result)
+            roi2 = self.vid_manager.get_roi(2)[0]
+            cv2.imwrite(f"{self.output_path}{os.sep}roi2.png", roi2)
+
+    def check_extraction_status(self):
+        if self.extraction_process_flag:
+            self.calculate_threshold()
+            return
+        elif self.extraction_error_flag:
+            return
+        try:
+            message = self.queue.get(block=False)
+        except Empty:
+            pass
+        else:
+            if message.startswith("Progress"):
+                progress = message.split(" ")[1].split("/")
+                self.progress_bar['value'] = int(progress[0])/int(progress[1]) * 100
+            elif message.startswith("frame_delta_t"):
+                f_delta_t = float(message.split("=")[1].strip())
+                self.vid_manager.set_frame_duration(f_delta_t)
+            elif message == "Process successful":
+                self.output_msg.config(text="Process successful", fg="green")
+                self.progress_bar['value'] = 100
+                self.control_btns.on_next()
+                self.extraction_process_flag = True
+            elif message.startswith("frame_rate"):
+                pass
+            else:
+                self.output_msg.config(text="Process unsuccessful", fg="red")
+                self.extraction_error_flag = True
+        finally:
+            self.after(20, self.check_extraction_status)
+
+    def calculate_threshold(self):
+        self.roi1_path = Path(f"{self.output_path}{os.sep}roi1.png")
+        self.roi2_path= Path(f"{self.output_path}{os.sep}roi2.png")
+        x = self.vid_manager.get_roi(1)[1][0]
+        y = self.vid_manager.get_roi(1)[1][1]
+        threshold_process = Process(target=calculate_threshold, args=(self.roi1_path, self.roi2_path, x, y)) #multiprocessing
+        threshold_process.start()
+        self.check_threshold_status()
+    
+    def check_threshold_status(self):
+        if self.threshold_process_flag:
+            os.remove(self.roi1_path)
+            os.remove(self.roi2_path)
+            return
+        elif self.threshold_error_flag:
+            return
+        try:
+            message = self.queue.get(block=False)
+        except Empty:
+            pass
+        else:
+            if message.startswith("Threshold Amount"):
+                threshold = message.split(" ")[1]
+                print(threshold)
+                self.vid_manager.set_threshold(int(threshold))
+                self.threshold_process_flag = True
+            else:
+                self.output_msg.config(text="Process unsuccessful", fg="red")
+                self.error_flag = True
+        finally:
+            self.after(100, self.check_threshold_status)
+
+    def can_next(self):
+        if self.vid_manager.get_output_path() and self.extraction_process_flag \
+            and self.threshold_process_flag:
+            return True
+        return False
